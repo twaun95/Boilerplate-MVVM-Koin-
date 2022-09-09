2eb3e476c748bea04d6472477d332575d63b175e
taewaun <taewaunkwon@gmail.com>
2022-09-09 22:38:43 +0900

    boiler-plate

--- app/src/main/java/com/twaun95/boilerplatemvvmkoin/MyApplication.kt
+++ app/src/main/java/com/twaun95/boilerplatemvvmkoin/MyApplication.kt
@@ -2,12 +2,17 @@ package com.twaun95.boilerplatemvvmkoin
 
 import android.app.Application
 import com.twaun95.boilerplatemvvmkoin.di.modules.DataModule
+import com.twaun95.boilerplatemvvmkoin.di.modules.LogModule
 import com.twaun95.boilerplatemvvmkoin.di.modules.ViewModelModule
+import com.twaun95.core.logger.ActivityLifecycleLogger
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class MyApplication : Application() {
+
+    private val activityLifeCycleLogger by inject<ActivityLifecycleLogger>()
 
     override fun onCreate() {
         super.onCreate()
@@ -15,9 +20,11 @@ class MyApplication : Application() {
             androidLogger()
             androidContext(this@MyApplication)
             modules(
+                LogModule.module,
                 ViewModelModule.module,
                 DataModule.module
             )
         }
+        registerActivityLifecycleCallbacks(activityLifeCycleLogger)
     }
 }
