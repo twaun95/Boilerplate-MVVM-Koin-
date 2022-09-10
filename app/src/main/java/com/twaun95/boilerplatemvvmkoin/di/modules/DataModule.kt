ff9b99c401d7e82c602a9d5a107e746a78f45ece
taewaun <taewaunkwon@gmail.com>
2022-09-10 17:57:18 +0900

    boiler-plate

--- app/src/main/java/com/twaun95/boilerplatemvvmkoin/di/modules/DataModule.kt
+++ app/src/main/java/com/twaun95/boilerplatemvvmkoin/di/modules/DataModule.kt
@@ -1,13 +1,16 @@
 package com.twaun95.boilerplatemvvmkoin.di.modules
 
+import com.twaun95.boilerplatemvvmkoin.data.local.AppSharedPreference
 import com.twaun95.boilerplatemvvmkoin.data.remote.MyRepository
 import com.twaun95.boilerplatemvvmkoin.di.KoinModule
+import org.koin.android.ext.koin.androidApplication
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 object DataModule : KoinModule {
     override val module: Module
         get() = module {
+            single { AppSharedPreference(androidApplication()) }
             single { MyRepository() }
         }
 }
